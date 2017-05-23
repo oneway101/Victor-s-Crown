b1244d31d6741d8a16cb093886a39ea9307f3e9e
oneway101 <oneway.first@gmail.com>
2017-05-23 18:33:51 -0400

    initial setup

---
+++ Victor's Crown/ProfileController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Victor's Crown
+//
+//  Created by Ha Na Gill on 5/19/17.
+//  Copyright © 2017 cosmostream. All rights reserved.
+//
+
+import UIKit
+import CoreGraphics
+
+class ProfileController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+//    let img = renderer.image { ctx in
+//        ctx.cgContext.setFillColor(UIColor.red.cgColor)
+//        ctx.cgContext.setStrokeColor(UIColor.green.cgColor)
+//        ctx.cgContext.setLineWidth(10)
+//        
+//        let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+//        ctx.cgContext.addRect(rectangle)
+//        ctx.cgContext.drawPath(using: .fillStroke)
+//    }
+    
+}
+
